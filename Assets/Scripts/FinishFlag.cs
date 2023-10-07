ceb12a2f57a0269abeee6cfbe38860e619aceb87
AxelX231 <133662155+AxelX231@users.noreply.github.com>
2023-10-07 20:16:38 +0300

    FinishFlag added

---
+++ Assets/Scripts/FinishFlag.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishFlag : MonoBehaviour
+{
+    private AudioSource finishSound;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        finishSound = GetComponent<AudioSource>;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+            finishSound.Play();
+        CompleteLevel();
+
+    }
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+  
+}
